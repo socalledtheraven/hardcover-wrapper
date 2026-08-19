4d8a21b7cc1d7110706943ad574fb665784dd3bc
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 22:47:21 +0100

    add notification fields

--- src/notification.rs
+++ src/notification.rs
@@ -1,4 +1,19 @@
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
+use crate::user::User;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct NotificationDelivery {}
+pub(crate) struct NotificationDelivery {
+    created_at: OffsetDateTime,
+    description: String,
+    id: u32,
+    link: Option<String>,
+    link_text: Option<String>,
+    notification_deliveries: Vec<NotificationDelivery>,
+    notification_type_id: u32,
+    notifierUser: User,
+    notifier_user_id: u32,
+    priority: Option<u32>,
+    title: String,
+    uid: String,
+}
