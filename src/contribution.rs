52368beeedecb88c710c8b62e7cdee3ff9c9cbf4
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 19:15:51 +0100

    add contribution fields

--- src/contribution.rs
+++ src/contribution.rs
@@ -1,4 +1,35 @@
 use serde::{Deserialize, Serialize};
+use time::PlainDateTime;
+use crate::author::Author;
+use crate::book::Book;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Contribution {}
+pub(crate) struct Contribution {
+    author: Option<Author>,
+    author_id: u32,
+    book: Option<Book>,
+    contributable_id: u32,
+    contributable_type: ContributableType,
+    contribution: Option<ContributionRole>,
+    created_at: PlainDateTime,
+    id: u32,
+    updated_at: PlainDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+enum ContributableType {
+    Book,
+    Edition,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+enum ContributionRole {
+    Author,
+    Illustrator,
+    Translator,
+    Editor,
+    Narrator,
+    Foreword,
+    Afterword,
+    CoverArtist
+}
