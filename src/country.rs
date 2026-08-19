99b124013a1da172e0acd2309e7346e1c8289e16
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 22:28:55 +0100

    add country fields

--- src/country.rs
+++ src/country.rs
@@ -1,4 +1,22 @@
 use serde::{Deserialize, Serialize};
+use time::PlainDateTime;
+use crate::edition::Edition;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Country {}
+pub(crate) struct Country {
+    code2: Option<String>,
+    code3: Option<String>,
+    created_at: PlainDateTime,
+    editions: Vec<Edition>,
+    id: u32,
+    intermediate_region: Option<String>,
+    intermediate_region_code: Option<String>,
+    iso_3166: Option<String>,
+    name: Option<String>,
+    phone_code: Option<String>,
+    region: Option<String>,
+    region_code: Option<String>,
+    sub_region: Option<String>,
+    sub_region_code: Option<String>,
+    updated_at: PlainDateTime,
+}
