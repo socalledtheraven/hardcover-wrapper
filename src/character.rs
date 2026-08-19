af2872b9dab7c712be141f3ef2210522a51f8b08
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 19:12:21 +0100

    add character fields

--- src/character.rs
+++ src/character.rs
@@ -1,4 +1,35 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use time::PlainDateTime;
+use crate::book::Book;
+use crate::contribution::Contribution;
+use crate::enums::{Gender, RecordState};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Character {}
+pub(crate) struct Character {
+    biography: Option<String>,
+    book_characters: Vec<Book>,
+    books_count: u64,
+    // todo!
+    cached_tags: Value,
+    canonical: Option<Box<Character>>,
+    canonical_books_count: u64,
+    canonical_id: Option<u32>,
+    contributions: Vec<Contribution>,
+    created_at: PlainDateTime,
+    gender_id: Option<Gender>,
+    has_disability: Option<bool>,
+    id: u32,
+    // todo!
+    image_id: Option<u32>,
+    is_lgbtq: Option<bool>,
+    is_poc: Option<bool>,
+    locked: Option<bool>,
+    name: String,
+    object_type: String,
+    openlibrary_url: Option<String>,
+    slug: String,
+    state: RecordState,
+    updated_at: PlainDateTime,
+    user_id: Option<u32>,
+}
