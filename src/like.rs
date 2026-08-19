0aab6b0fb7356fcf34fd4eff7b7d4c978dcb9d75
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 22:39:48 +0100

    add like fields

--- src/like.rs
+++ src/like.rs
@@ -1,4 +1,20 @@
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
+use crate::activity::Activity;
+use crate::list::List;
+use crate::user::User;
+use crate::user_book::UserBook;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Like {}
+pub(crate) struct Like {
+    activity: Option<Activity>,
+    created_at: Option<OffsetDateTime>,
+    followers: Vec<User>,
+    id: u32,
+    likeable_id: u32,
+    likeable_type: String,
+    list: Option<List>,
+    user: User,
+    user_book: Option<UserBook>,
+    user_id: u32,
+}
