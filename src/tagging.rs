602c8ccc143310410d235bf866306c7962629969
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 22:55:45 +0100

    add tagging fields

--- src/tagging.rs
+++ src/tagging.rs
@@ -1,4 +1,15 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Tagging {}
+pub(crate) struct Tagging {
+    count: u32,
+    id: u32,
+    slug: String,
+    tag: String,
+    // todo!
+    tag_category: Value,
+    tag_category_id: u32,
+    // todo!
+    taggings: Vec<Value>,
+}
