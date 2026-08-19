e26aea72549e8cf647d5ecc78a04d1751e6ac52a
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 22:37:33 +0100

    add language fields

--- src/language.rs
+++ src/language.rs
@@ -1,4 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Language {}
+pub(crate) struct Language {
+    code2: Option<String>,
+    code3: Option<String>,
+    id: u32,
+    language: String,
+}
