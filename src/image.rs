73072eaa89c96244529c7d4dbbd3eecc396075a6
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-08-19 22:36:12 +0100

    modify image fields

--- src/image.rs
+++ src/image.rs
@@ -3,23 +3,33 @@ use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub(crate) struct Image {
-    color: String,
-    color_name: String,
-    height: u64,
+    color: Option<String>,
+    // todo!
+    colors: Option<Value>,
+    color_name: Option<String>,
+    height: Option<u64>,
     id: u64,
-    url: String,
-    width: u64,
+    imageable_id: Option<u64>,
+    // todo!
+    imageable_type: Option<Value>,
+    ratio: Option<f64>,
+    url: Option<String>,
+    width: Option<u64>,
 }
 
 impl Image {
     pub(crate) fn new(resp: Value) -> Self {
         Image {
-            color: resp["color"].as_str().unwrap().to_string(),
-            color_name: resp["color_name"].as_str().unwrap().to_string(),
-            height: resp["height"].as_u64().unwrap(),
-            id: resp["id"].as_u64().unwrap(),
-            url: resp["url"].as_str().unwrap().to_string(),
-            width: resp["width"].as_u64().unwrap(),
+            color: resp["color"].as_str().map(|s| s.to_string()),
+            colors: None,
+            color_name: resp["color_name"].as_str().map(|s| s.to_string()),
+            height: resp["height"].as_u64(),
+            id: resp["id"].as_u64().expect("REASON"),
+            imageable_id: None,
+            imageable_type: None,
+            ratio: None,
+            url: resp["url"].as_str().map(|s| s.to_string()),
+            width: resp["width"].as_u64(),
         }
     }
 }
