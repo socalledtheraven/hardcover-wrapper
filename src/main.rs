ed0b9597048f903bc7da1eb3c69c68ea8dac4eff
socalledtheraven <cassie.dalrymple3@gmail.com>
2026-09-04 21:25:39 +0100

    add prompt init

--- src/main.rs
+++ src/main.rs
@@ -60,8 +60,11 @@ async fn main() -> Result<(), reqwest::Error> {
     // let list = list::List::from_list_id(11325).await?;
     // println!("{list:#?}");
 
-    let notification = notification::Notification::from_notification_id(440096).await?;
-    println!("{notification:#?}");
+    // let notification = notification::Notification::from_notification_id(440096).await?;
+    // println!("{notification:#?}");
+
+    let prompt = prompt::Prompt::from_prompt_id(122).await?;
+    println!("{prompt:#?}");
 
     Ok(())
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,33 +1,95 @@
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use time::OffsetDateTime;
-use crate::enums::PrivacySetting;
-use crate::user::User;
+use crate::graphql::{graphql_req, GraphQLResponse};
+
+const QUERY_FIELDS: &str = r#"
+answers_count
+books_count
+created_at
+description
+featured
+id
+privacy_setting_id
+question
+slug
+user_id
+users_count
+"#;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub(crate) struct Prompt {
     answers_count: u64,
     books_count: u64,
-    created_at: Option<OffsetDateTime>	,
+    created_at: Option<OffsetDateTime>,
     description: String,
     featured: bool,
-    // todo!
-    followed_prompts: Vec<Value>,
-    followers: Vec<User>,
     id: u64,
-    privacy_setting: PrivacySetting,
     privacy_setting_id: u64,
-    prompt_answers: Vec<PromptAnswer>,
-    prompt_books: Vec<PromptBook>,
     question: String,
     slug: String,
-    user: User,
     user_id: u64,
     users_count: u64,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct PromptAnswer {}
+impl Prompt {
+    pub(crate) async fn from_prompt_id(prompt_id: u64) -> Result<Self, reqwest::Error> {
+        let query = r#"
+        query GetAuthor($prompt: Int!) {
+          prompts_by_pk(id: $prompt) {"#.to_string() + QUERY_FIELDS + r#"
+          }
+        }
+        "#;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct PromptBook {}
+        Self::from_data(query, prompt_id).await
+    }
+
+    async fn from_data<T: ToString>(query: String, user_data: T) -> Result<Self, reqwest::Error> {
+        let mut vars = HashMap::new();
+        vars.insert("prompt", user_data.to_string());
+
+        Self::new(query, vars).await
+    }
+
+    async fn new(query: String, vars: HashMap<&str, String>) -> Result<Self, reqwest::Error> {
+        let resp = graphql_req(query, vars).await?;
+
+        let data = &resp["data"]["prompts_by_pk"];
+
+        Ok(Prompt{
+            answers_count: {
+                data.get_u64("answers_count").unwrap()
+            },
+            books_count: {
+                data.get_u64("books_count").unwrap()
+            },
+            created_at: {
+                data.get_offsetdt("created_at")
+            },
+            description: {
+                data.get_str("description").unwrap()
+            },
+            featured: {
+                data.get_bool("featured")
+            },
+            id: {
+                data.get_u64("id").unwrap()
+            },
+            privacy_setting_id: {
+                data.get_u64("privacy_setting_id").unwrap()
+            },
+            question: {
+                data.get_str("question").unwrap()
+            },
+            slug: {
+                data.get_str("slug").unwrap()
+            },
+            user_id: {
+                data.get_u64("user_id").unwrap()
+            },
+            users_count: {
+                data.get_u64("users_count").unwrap()
+            },
+        })
+    }
+}
